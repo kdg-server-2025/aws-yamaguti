b0e821ad009bb93f5cce9f04beb88aca5e6bee7d
merumerume <keita.yamaguti0117@gmail.com>
2025-06-07 15:47:30 +0900

    4回目

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_sns_topic" "budget_alerts" {
   name = "budget-alerts"
 }
 
-resource "aws_sns_topic_subscription" "email_subscription_yamakawa" {
+resource "aws_sns_topic_subscription" "email_subscription_yamaguti" {
   topic_arn = aws_sns_topic.budget_alerts.arn
   protocol  = "email"
   endpoint  = "keita.yamaguti0117@gmail.com"
